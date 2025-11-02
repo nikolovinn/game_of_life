7ae4e14d059a7e4ca61210d83661cc0c81fae054
Ivan Nikolov <i.nikolov@diewerkbank.eu>
2025-11-02 18:04:20 +0100

    Implements BlinkerGenerator

---
+++ src/GridGenerators/BlinkerGenerator.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\GridGenerators;
+
+use App\Grid;
+
+/**
+ * Provides helper methods to seed a 'Glider' pattern
+ *
+ */
+final class BlinkerGenerator extends AbstractGenerator
+{
+    /**
+     * Generates a glider pattern in the center of a grid
+     * with the given dimensions.
+     *
+     * @param int $width
+     * @param int $height
+     * @return bool
+     */
+    public function generate(): Grid
+    {
+        $gliderPattern = $this->getPattern();
+        $startPosition = $this->getStartPositionForCenterPlacement();
+
+        return self::seedAt($this->grid, $startPosition[1], $startPosition[1], $gliderPattern);
+    }
+
+    protected function getPattern(): array
+    {
+        return [
+            [0, 0], [0, 1], [0, 2],
+        ];
+    }
+}
